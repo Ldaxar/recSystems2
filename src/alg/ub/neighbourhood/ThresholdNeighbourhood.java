991536b98617e5557d1bbcbdb41ae6b8dcd1d26f
Stargarth <adamek1221@gmail.com>
2020-03-06 13:37:50 +0000

    changes

--- src/alg/ub/neighbourhood/ThresholdNeighbourhood.java
+++ src/alg/ub/neighbourhood/ThresholdNeighbourhood.java
@@ -1,6 +1,11 @@
 package alg.ub.neighbourhood;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import profile.Profile;
 import similarity.SimilarityMap;
+import util.ScoredThingDsc;
 
 public class ThresholdNeighbourhood extends Neighbourhood{
 
@@ -18,7 +23,20 @@ public class ThresholdNeighbourhood extends Neighbourhood{
 	@Override
 	public void computeNeighbourhoods(SimilarityMap simMap) {
 		// TODO Auto-generated method stub
-		
+		for (Integer userId: simMap.getIds()) {
+			Set<ScoredThingDsc> thresholdEntries = new HashSet<ScoredThingDsc>(); 
+			
+			Profile profile = simMap.getSimilarities(userId); // get the user similarity profile
+			if (profile != null) {
+				for (Integer id: profile.getIds()) { // iterate over each user in the profile
+					double sim = profile.getValue(id);
+					if (sim>threshold) {
+						this.add(userId, id);
+					//	thresholdEntries.add(new ScoredThingDsc(sim, id));
+					}
+				}
+			}
+		}
 	}
 
 }
--- src/alg/ub/predictor/WeightedAveragePredictor.java
+++ src/alg/ub/predictor/WeightedAveragePredictor.java
@@ -7,7 +7,7 @@ import alg.ub.neighbourhood.Neighbourhood;
 import profile.Profile;
 import similarity.SimilarityMap;
 
-public class DeviationFromUserMeanPredictor implements Predictor {
+public class WeightedAveragePredictor implements Predictor {
 
 	@Override
 	public Double getPrediction(Integer userId, Integer itemId, Map<Integer, Profile> userProfileMap,
@@ -23,16 +23,13 @@ public class DeviationFromUserMeanPredictor implements Predictor {
 		{
 			Double rating = userProfileMap.get(neighbour).getValue(itemId); // get the neighbour's rating for the target item
 			Profile profile = simMap.getSimilarities(userId);
-			if (profile != null && rating != null) {
-				double sim = simMap.getSimilarity(userId, neighbour);
-				if (userProfileMap.get(neighbour) == null)
-					continue;
-				
-				s1+=(sim*(rating-userProfileMap.get(neighbour).getMeanValue()));
+			if (profile != null) {
+				double sim = profile.getValue(neighbour);
+				s1+=(sim*rating);
 				s2+=sim;
 			}
 		}
-		return s2 != 0d ? userProfileMap.get(userId).getMeanValue()+(s1/s2) : 0d;
+		return s2 != 0d ? s1/s2 : 0d;
 	}
 
 }
